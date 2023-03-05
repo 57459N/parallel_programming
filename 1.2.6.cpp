ae3d9afb3d209b46fbba4e0f10a0b008bbae82ca
Stas Indyukov <stasun008@gmail.com>
2023-03-05 18:29:23 +0300

    1_4_14 done

---
+++ 1.2.6.cpp
@@ -0,0 +1,46 @@
+#include "tasks.h"
+
+int main1_2_6(int argc, char **argv) {
+    int rank;
+    int size;
+//
+    int k = 0;
+
+    int *buf = nullptr;
+
+    const int a = 5, b = 5;
+    int M[a][b];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    for (int i = 0; i < a; ++i) {
+        for (int j = 0; j < b; ++j) {
+            M[i][j] = rank;
+        }
+    }
+
+    int *sendbuf = new int[a];
+    for (int i = 0; i < a; ++i) {
+        sendbuf[i] = M[k][i];
+    }
+
+
+    buf = new int[size * a]{};
+
+    MPI_Gather(M, 5, MPI_INT, buf, 5, MPI_INT, k, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        for (int i = 0; i < a; ++i) {
+            for (int j = 0; j < size; ++j) {
+                std::cout << buf[i * a + j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    delete[] buf;
+
+    MPI_Finalize();
+}
--- 1.4.14.cpp
+++ 1.4.14.cpp
@@ -1,24 +1,52 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "openmp-use-default-none"
 
-#include "tasks.h"
 #include <chrono>
+#include <list>
+#include "vector"
+
+#include "tasks.h"
 
 template<typename T>
-void fill_matrix(T *matrix, int n, int m) {
-    for (int i = 0; i < n; ++i)
+void fill_vector(std::vector<std::vector<T>> &matrix, int n, int m) {
+
+    for (int i = 0; i < n; ++i) {
+        matrix[i].resize(n);
         for (int j = 0; j < m; ++j)
-            matrix[n * i + j] = rand() % 10;
+            matrix[i][j] = rand() % 10;
+    }
 }
 
+
 template<typename T>
-void print_matrix(T *matrix, int n, int m) {
+void print_matrix(const std::vector<std::vector<T>> &matrix, int n, int m) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j)
-            std::cout << std::setw(20) << matrix[n * i + j];
+            std::cout << std::setw(15) << matrix[i][j];
         std::cout << std::endl;
     }
 }
+
+template<typename T>
+void fill_pointer(T **matrix, int n, int m) {
+    matrix = new double *[n];
+    for (int i = 0; i < n; ++i) {
+        matrix[i] = new double[m];
+        for (int j = 0; j < m; ++j)
+            matrix[i][j] = rand() % 10;
+    }
+}
+
+
+template<typename T>
+void print_matrix(T **matrix, int n, int m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j)
+            std::cout << std::setw(15) << matrix[i][j];
+        std::cout << std::endl;
+    }
+}
+
 
 int main1_4_14(int argc, char **argv) {
 
@@ -27,8 +55,8 @@ int main1_4_14(int argc, char **argv) {
     int thread_num = atoi(argv[1]);
     omp_set_num_threads(thread_num);
 
-    std::srand(0);
-    int n_size = 3;
+    std::srand(time(nullptr));
+    int n_size = 100;
 
     /*  with srand(0)
         Matrix A:
@@ -47,21 +75,44 @@ int main1_4_14(int argc, char **argv) {
              92            103            135
      */
 
-    auto *matrix_A = new double[n_size * n_size];
+//    std::vector<std::vector<double>> matrix_A(n_size);
+//    std::vector<std::vector<double>> matrix_B(n_size);
+//
+//    fill_vector(matrix_B, n_size, n_size);
+//    fill_vector(matrix_A, n_size, n_size);
+//
+//    std::vector<std::vector<double>> matrix_C_1(n_size);
+//    std::vector<std::vector<double>> matrix_C_2(n_size);
+//
+//    for (auto &el: matrix_C_1) {
+//        el.resize(n_size);
+//    }
+//    for (auto &el: matrix_C_2) {
+//        el.resize(n_size);
+//    }
 
-    auto *matrix_B = new double[n_size * n_size];
 
-    fill_matrix(matrix_A, n_size, n_size);
-    fill_matrix(matrix_B, n_size, n_size);
+    double **matrix_A = nullptr;
+    double **matrix_B = nullptr;
 
+    fill_pointer(matrix_B, n_size, n_size);
+    fill_pointer(matrix_A, n_size, n_size);
 
-    std::cout << "Matrix A:\n";
-    print_matrix(matrix_A, n_size, n_size);
-    std::cout << "\nMatrix B:\n";
-    print_matrix(matrix_B, n_size, n_size);
-    std::cout << std::endl;
+    auto matrix_C_1 = new double *[n_size];
+    auto matrix_C_2 = new double *[n_size];
 
-    auto matrix_C = new double[n_size * n_size]{};
+    for (int i = 0; i < n_size; ++i) {
+        matrix_C_1[i] = new double[n_size]{};
+        matrix_C_2[i] = new double[n_size]{};
+    }
+
+    if (n_size < 10) {
+        std::cout << "Matrix A:\n";
+        print_matrix(matrix_A, n_size, n_size);
+        std::cout << "\nMatrix B:\n";
+        print_matrix(matrix_B, n_size, n_size);
+        std::cout << std::endl;
+    }
 
     auto start_point = std::chrono::high_resolution_clock::now();
 
@@ -73,10 +124,13 @@ int main1_4_14(int argc, char **argv) {
             for (int j = 0; j < n_size; ++j) {
                 double sum = 0;
                 for (int k = 0; k < n_size; ++k) {
-                    sum += matrix_A[i * n_size + k] * matrix_A[k * n_size + j];
-                    sum += matrix_B[i * n_size + k] * matrix_B[k * n_size + j];
+                    sum += matrix_A[i][k] * matrix_A[k][j];
+                    sum += matrix_B[i][k] * matrix_B[k][j];
                 }
-                matrix_C[i * n_size + j] = sum;
+#pragma omp critical
+                {
+                    matrix_C_2[i][j] = sum;
+                };
             }
 
         }
@@ -88,14 +142,23 @@ int main1_4_14(int argc, char **argv) {
     auto end = std::chrono::time_point_cast<std::chrono::microseconds>(end_point).time_since_epoch().count();
 
 
-    std::cout << (end - start) << " microseconds\n";
+    std::cout << (end - start) << " microseconds offset\n";
 
-    print_matrix(matrix_C, n_size, n_size);
+    bool same = true;
+    for (int i = 0; i < n_size; ++i) {
+        for (int j = 0; j < n_size; ++j)
+            if (matrix_C_1[i][j] != matrix_C_2[i][j]) {
+                same = false;
+                break;
+            }
+        if (!same)
+            break;
+
+    }
+
+    std::cout << (same ? "same" : "not same") << std::endl;
 
 
-    delete[] matrix_A;
-    delete[] matrix_B;
-    delete[] matrix_C;
     return 0;
 }
 
